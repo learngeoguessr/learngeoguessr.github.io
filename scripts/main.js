c972cb9793922d92d9325a1c0d326f57d8c8e0c4
learngeoguessr <throwawayegi@gmail.com>
2022-09-14 20:10:41 +0200

    Sidebar added

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,20 @@
+var mini = true;
+
+function toggleSidebar() {
+  if (mini) {
+    console.log("opening sidebar");
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    this.mini = false;
+  } else {
+    console.log("closing sidebar");
+    document.getElementById("mySidebar").style.width = "85px";
+    document.getElementById("main").style.marginLeft = "85px";
+    this.mini = true;
+  }
+}
+
 function fadeOut(){
-    const main = document.getElementById("main1");
-    main.classList.add("animate__animated", "animate__bounceOutDown");
-}
-
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  const main = document.getElementById("main1");
+  main.classList.add("animate__animated", "animate__bounceOutDown");
 }
